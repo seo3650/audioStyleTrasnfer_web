b840c99be9376f874d7f2f0c83f9eefbcfa95862
sonjbin <sonjbin@kaist.ac.kr>
2020-12-11 23:06:11 +0900

    Update index.py
    add util functions

--- backend/index.py
+++ backend/index.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
 import shutil
+from pydub import AudioSegment
+import torch
+from transfer.model import Generator
+import subprocess
+
 
 app = Flask(__name__)
 
@@ -25,6 +30,36 @@ def download_file():
     return send_file('/home/ubuntu/audio_style_transfer/backend/convertedFiles/' + secure_filename(file_name),
                      attachment_filename=file_name,
                      as_attachment=True)
+
+"""
+Load model
+"""
+def load_info(state_dict_path):
+    global model
+    model =  Generator()
+    model.load_state_dict(torch.load(state_dict_path))
+    model.eval()
+"""
+Split music to mr and vocal
+
+You should make directory named output
+file loaded in output like
+- accompaniment.wav
+- vocal.wav
+"""
+def split_mr(music_file):
+    subprocess.call(["spleeter", 'separate', '-i', music_file, '-o', 'output'])
+
+"""
+Requirement: pydub
+usage - pip install pydub
+"""
+def merge_mr(mr_file, converted_file):
+    mr = AudioSegment.from_wav(mr_file)
+    output = AudioSegment.from_wav(converted_file)
+    combined_sounds = output.overlay(mr)
+    combined_sounds.export("./audio_merged.wav", format="wav")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
